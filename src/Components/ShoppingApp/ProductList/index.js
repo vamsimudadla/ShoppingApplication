e90552e2df7dbe582f5e285b5404cc0f6fc1d6c7
vamsimudadla <mudadlavamsi@gmail.com>
2019-07-22 18:11:54 +0530

    added shoppingStore Header and its styles

--- src/Components/ShoppingApp/ProductList/index.js
+++ src/Components/ShoppingApp/ProductList/index.js
@@ -8,14 +8,33 @@ class ProductList extends Component {
   render() {
     const shoppingStore = this.props.shoppingStore;
     return (
-      <div className="productsList">
-        {shoppingStore.getSelectedSizeProducts.length === 0 ? (
-          <span>No Products Available</span>
-        ) : (
-          shoppingStore.getSelectedSizeProducts.map(product => (
-            <Product product={product} />
-          ))
-        )}
+      <div className="HeaderWithProductList">
+        <div className="shoppingStoreHeader">
+          <span>
+            {shoppingStore.getSelectedSizeProducts.length +
+              " " +
+              "Product(s) found"}
+          </span>
+          <div className="orderByDropDown">
+            <span>Order by </span>
+            <select name="priceOrder" className="dropDown">
+              <option value="" disabled selected hidden>
+                Select
+              </option>
+              <option value="Lowest To Highest">Lowest To Highest</option>
+              <option value="Highest To Lowest">Highest To Lowest</option>
+            </select>
+          </div>
+        </div>
+        <div className="productsList">
+          {shoppingStore.getSelectedSizeProducts.length === 0 ? (
+            <span>No Products Available</span>
+          ) : (
+            shoppingStore.getSelectedSizeProducts.map(product => (
+              <Product product={product} />
+            ))
+          )}
+        </div>
       </div>
     );
   }
